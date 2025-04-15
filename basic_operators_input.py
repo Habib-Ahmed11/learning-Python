7cfc015e1f13edfee2b2fe4cddcfe12070ae4909
Habib-Ahmed11 <159695211+Habib-Ahmed11@users.noreply.github.com>
2025-04-15 11:43:09 +0500

    Add basic python code for operators, type conversions and input

---
+++ basic_operators_input.py
@@ -0,0 +1,84 @@
+#=====================
+#Arithemetic Operators
+#=====================
+print("Arithmetic Operators")
+a = 5
+b = 3
+
+print(a + b)
+print(a - b)
+print(a / b)
+print(a * b)
+
+#=====================
+#Comparison Operators
+#=====================
+print("Comparison Operators")
+a = 50
+b = 20
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+#=====================
+#Assignment Operators
+#=====================
+print("Assignment Operators")
+a = 10
+b = 5
+
+a += 5
+print(a)
+a -= 5
+print(a)
+a /= 5
+print(a)
+a *= 5
+print(a)
+a %= 5
+print(a)
+a **= 5
+print(a)
+
+#=====================
+#Logical Operators
+#=====================
+print("Logical Operators")
+print(not True)
+print(not False)
+
+a = 15
+b = 10
+print((a > b) and (a == b))
+print((a > b) or (a == b))
+print(not a > b)
+
+a = int('5')
+b = 5
+
+print(a + b)
+
+a = float("5.5")
+b = 4.55
+print(a + b)
+
+a = 3.14
+a = str(a)
+print(type(a))
+
+#=====================
+#input in Pyton
+#=====================
+name = input("Enter your name: ")
+print("Your name is ", name)
+age = int(input("Enter your age: "))
+print("Your age is ", age)
+print(type(age))
+marks = float(input("Enter your marks "))
+print("Your marks are ", marks)
+print(type(marks))
+
